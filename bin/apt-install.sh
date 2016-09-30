76b513937b4991507805a7570566317f3594298f
Crispin Wellington <retrogradeorbit@gmail.com>
2016-09-30 21:59:38 +0800

    apt package installer

---
+++ bin/apt-install.sh
@@ -0,0 +1,18 @@
+add-apt-repository ppa:webupd8team/java
+
+apt-get update
+apt-get install oracle-java8-installer build-essential emacs \
+	libpango1.0-0 libappindicator1 git pulseaudio dkms \
+	xmonad libghc-xmonad-contrib-dev xmobar stalonetray \
+	suckless-tools scrot cabal-install xcompmgr
+
+# xmonad stuff
+cabal update
+cabal install --global yeganesh
+
+# audio on modern intel hda audio laptop
+apt-add-repository ppa:ubuntu-audio-dev/alsa-daily
+apt-get install oem-audio-hda-daily-lts-xenial-dkms
+
+# keypress tools
+apt-get install xbacklight
